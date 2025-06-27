64e88683e1cafe393670b748839e9d74f218aa5a
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-28 00:55:40 +0530

    shell_final

--- 02-basic-installation.sh
+++ 02-basic-installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 USER=$(id -u)
-SCRIPT_NAME=$(ecgo $0 | cut -d "." -f1 )
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
 TIME_STAMP=$(date +%F-%H-%M-%S)
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log 
 echo "script name is $LOG_FILE"
