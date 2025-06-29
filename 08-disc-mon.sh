55f7363a3c90684b1cd72c1915dcf3757e958d68
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-29 07:42:44 +0530

    crontab files

--- 08-disc-mon.sh
+++ 08-disc-mon.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-disc_utilization= $(df -hT | grep xfs | awk -d '{print $6}' | cut -d "%" -f1)
+disc_utilization= $(df -hT | grep xfs )
+disc_threshold=
+
+while IFS= read -s LINE 
+do 
+    USAGE=$LINE |  awk -F " "  '{print $6F}' | cut -d "%" -f
+    FOLDER=$$LINE |  awk -F " "  '{print $NF}'
+    if [ $USAGE -gt $disc_threshold ]
+    then    
+        echo "$FOLDER is more the $disc_utilization , current usages is $USAGE"
+dond <<< $disc_utilization
+
+
