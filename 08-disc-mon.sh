8c91d2a5682eb65c9d2e3342091400ea4279906e
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-29 07:47:35 +0530

    crontab files

--- 08-disc-mon.sh
+++ 08-disc-mon.sh
@@ -3,7 +3,7 @@
 disc_utilization=$(df -hT | grep xfs )
 disc_threshold=5
 
-while IFS= read -s LINE 
+while IFS= read -r LINE 
 do 
     USAGE=$LINE |  awk -F " "  '{print $6F}' | cut -d "%" -f
     FOLDER=$$LINE |  awk -F " "  '{print $NF}'
