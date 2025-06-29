0dac7480fb5735d2e4bf4e7ea8a782a64f722125
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-29 08:30:30 +0530

    crontab files

--- 09-mem.sh
+++ 09-mem.sh
@@ -3,10 +3,10 @@
 mem_FREE=$(free -m | awk -F " " '{print $3}')
 MEM_THRESHOLD=300 # in mb 
 
-if [ $mem_FREE -gt $MEM_THRESHOLD ]
-then
-    echo "more space occupied $mem_FREE"
-else 
-    echo " we have some space $mem_FREE - $MEM_THRESHOLD"
-fi 
+ 
 
+while IFS= read -r LINE 
+do 
+echo $LINE 
+
+done <<< $mem_FREE
